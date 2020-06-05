2e37d5ae2aa7a70db4377b117c311388e28ff906
carlosmorin78 <carlosmorin78@gmail.com>
2020-06-05 10:43:58 -0500

    API config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ENV['API_URI'] = 'http://api.openweathermap.org/data/2.5/weather'
+ENV['API_KEY'] = '3fc82b390a874c7c422510d5488b0722'
+ENV['LOCAL_CITY'] = 'Monterrey,MX'
